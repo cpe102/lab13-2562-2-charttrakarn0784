75fac1cab11ef9889ad145ea6b531697df9e16d2
lab302-14 <lab302-14@labeng.local>
2020-02-11 17:12:12 +0700

    lab13

--- lab13_1.cpp
+++ lab13_1.cpp
@@ -11,35 +11,32 @@ void swap(T d[],int x,int y){
 
 template <typename T>
 void insertionSort(T d[],int N){
-	int temp;
 	for(int end = 1;end < N;end++)
 	{
 		
-		for(int i = 0; i < 10; i++) 
+		for(int i = 0; i < N; i++) 
 		{
 			if(i == end) cout << "[" << d[i] << "] ";
 			else cout << d[i] << " ";
 		}
 		cout << "=> ";
 		
-		temp = d[end];
+		int position = 1;
 		for(int j = end; j>0; j--)
 		{
-			
 			if(d[j] > d[j-1])
 			{
 			swap(d,j,j-1);
+			position = j-1;
 			}
-			else if(j == N-1) break;
 		}
 		
-		bool count = 0;
-		for(int k = 0; k < 10; k++)
+
+		for(int k = 0; k < N; k++)
 		{
-			if(d[k] == temp && count == 0) 
+			if(k == position) 
 			{
 				cout << "[" << d[k] << "] ";
-				count++;
 			}
 			else cout << d[k] << " ";
 		}		
